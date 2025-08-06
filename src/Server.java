f69b3e753c3592b61f97bff4605ad8b3e0eb75d8
Matheus Farias <matheusfarias3456@hotmail.com>
2025-08-06 20:46:50 -0300

    Setting Server

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,53 @@
+import javax.imageio.IIOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+
+        try {
+
+            while (!serverSocket.isClosed()) {
+
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected!");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+            }
+
+        } catch (IIOException e) {
+
+        }
+
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IIOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IIOException{
+
+        int port = 1234;
+        ServerSocket serverSocket = new ServerSocke(port);
+        Server server = new Server(serverSocket);
+        server.startServer();
+
+    }
 }
